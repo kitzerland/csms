a984790cdde8f793c10e960836d88064320ed22a
kitzerland <kitz.kitzerland@gmail.com>
2016-07-22 20:40:20 +0530

    index page. showing states deleted. fixed?

--- Core/View.php
+++ Core/View.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Core;
+
 /**
  * View
  */
@@ -11,33 +13,44 @@ class View {
      */
     public static function render($view, $args = []) {
         extract($args, EXTR_SKIP);
-        
+
         $file = "../App/Views/$view";
-        
-        if(is_readable($file)){
+
+        if (is_readable($file)) {
             require $file;
-        }else{
+        } else {
             throw new \Exception("$file not found");
         }
     }
-    
+
     /**
      * Render a view template using Twig
      * @param string $template The template file
      * @param array $args Associative array of data to display in the view
      * @return void
      */
-    public static function renderTemplate($template, $args = []){
+    public static function renderTemplate($template, $args = []) {
         static $twig = null;
-        
-        if($twig === null){
+        $args['baseURL'] = static::baseURL();
+        if ($twig === null) {
             $loader = new \Twig_Loader_Filesystem('../App/Views');
             $twig = new \Twig_Environment($loader);
         }
-        if(session_status() == PHP_SESSION_ACTIVE){
+        if (session_status() == PHP_SESSION_ACTIVE) {
             $args['session'] = $_SESSION;
         }
         echo $twig->render($template, $args);
+    }
+
+    private static function baseURL() {
+        global $system_name;
+        $protocol = '';
+        if (isset($_SERVER['HTTPS'])) {
+            $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https://" : "http://";
+        } else {
+            $protocol = 'http://';
+        }
+        return $protocol . $_SERVER['HTTP_HOST'];
     }
 
 }
--- public/index.php
+++ public/index.php
@@ -55,14 +55,14 @@ $router = new Core\Router();
 $router->add('login', ['controller' => 'LoginController', 'action' => 'index', 'namespace' => 'User']);
 $router->add('', ['controller' => 'HomeController', 'action' => 'index']);
 $router->add('index', ['controller' => 'HomeController', 'action' => 'index']);
-$router->add('register', ['controller' => 'RegistrationController', 'action' => 'index', 'namespace' => 'Student']);
+$router->add('student/register', ['controller' => 'RegistrationController', 'action' => 'index', 'namespace' => 'Student']);
 
 
 /**
  * Post Requests
  */
 $router->post('login', ['controller' => 'LoginController', 'action' => 'login', 'namespace' => 'User']);
-$router->post('register', ['controller' => 'RegistrationController', 'action' => 'register', 'namespace' => 'Student']);
+$router->post('student/register', ['controller' => 'RegistrationController', 'action' => 'register', 'namespace' => 'Student']);
 
 
 
